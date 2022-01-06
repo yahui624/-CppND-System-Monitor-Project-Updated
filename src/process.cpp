b6511edb00f1f3370034da7bc576cb0616ac745c
root <yahui624@vt.edu>
2022-01-06 07:47:26 +0000

    working

--- src/process.cpp
+++ src/process.cpp
@@ -5,30 +5,45 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
 // TODO: Return this process's ID
-int Process::Pid() { return 0; }
+Process::Process(int pid) {
+    pid_ = pid;
+}
+
+int Process::Pid() { 
+    return pid_;
+ }
 
 // TODO: Return this process's CPU utilization
 float Process::CpuUtilization() { 
-    return cpuUtilization_ = Linux
+    return cpuUtilization_;
  }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() { 
+    return LinuxParser::Command(pid_);
+}
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() { 
+    return LinuxParser::Ram(pid_); 
+ }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() { 
+    return LinuxParser::User(pid_); 
+}
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() { 
+    return LinuxParser::UpTime(pid_); 
+ }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
